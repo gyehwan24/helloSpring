5a2ea7e7f734a77b228a5a6d9205ae76ab1bbfed
Gyehwan Baek <b99hwan@gmail.com>
2022-09-02 01:01:13 +0900

    Feat: AOP

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
-    Optional<Member> findByname(String name);
+    @Override
+    Optional<Member> findByName(String name);
 }
