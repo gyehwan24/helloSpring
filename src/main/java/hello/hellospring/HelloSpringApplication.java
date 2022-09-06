e22c01785f16b4ab73c73120a39dd637b59ee0f6
Gyehwan Baek <b99hwan@gmail.com>
2022-09-06 23:42:55 +0900

    fix: 버전 변경

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,8 +3,11 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//자바 버전 변경
+
 @SpringBootApplication
 public class HelloSpringApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
